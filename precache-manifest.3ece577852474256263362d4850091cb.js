8b089f5254d8214724203555f5b26ddac4cd1dd3
Citlalli DMG <citlallidmg@gmail.com>
2018-12-11 23:25:51 -0600

    Updates

---
+++ precache-manifest.3ece577852474256263362d4850091cb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "435034a00c1a92c3a72f",
+    "url": "/wiki-marvel/static/js/runtime~main.435034a0.js"
+  },
+  {
+    "revision": "cfa5e80693104c6ecba2",
+    "url": "/wiki-marvel/static/js/main.cfa5e806.chunk.js"
+  },
+  {
+    "revision": "9b2dbce23edc294068a5",
+    "url": "/wiki-marvel/static/js/1.9b2dbce2.chunk.js"
+  },
+  {
+    "revision": "4cd9d6c07d7bcc3ed6dbc3fa2d904442",
+    "url": "/wiki-marvel/index.html"
+  }
+];
