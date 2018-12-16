e845a372c28dcb03b4139f2e0e6f5ad876e23fad
Citlalli DMG <citlallidmg@gmail.com>
2018-12-16 17:40:25 -0600

    Updates

---
+++ precache-manifest.c72f55d64e89b256c53500010e76d4d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e053e1cad248c7f1c8e020ed06021f4",
+    "url": "/wikimarvel/static/media/Marvel_Wiki_Comics_logo.4e053e1c.png"
+  },
+  {
+    "revision": "909fd099004f589d9a54",
+    "url": "/wikimarvel/static/js/runtime~main.909fd099.js"
+  },
+  {
+    "revision": "71ac191722ab254a0488",
+    "url": "/wikimarvel/static/js/main.71ac1917.chunk.js"
+  },
+  {
+    "revision": "c8ceb18f350e08dae801",
+    "url": "/wikimarvel/static/js/1.c8ceb18f.chunk.js"
+  },
+  {
+    "revision": "71ac191722ab254a0488",
+    "url": "/wikimarvel/static/css/main.4c20faa0.chunk.css"
+  },
+  {
+    "revision": "a0d072305ab6a929746fb3f445c4f22d",
+    "url": "/wikimarvel/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19b1bf762f4e67ea9fa090ee0884db2b.js"
+  "/wikimarvel/precache-manifest.c72f55d64e89b256c53500010e76d4d5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/wikimarvel/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
