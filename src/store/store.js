0e29b5214e248e4f74af4b1b412709e8b1185ec2
Citlalli DMG <citlallidmg@gmail.com>
2018-12-19 18:43:03 -0600

    fixes problem with redux developer extension

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,18 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
+// dev tools middleware
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import { rootReducer, rootSaga } from "./modules";
 
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 
-// dev tools middleware
-const reduxDevTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
 // mount it on the Store
 export default createStore(
   rootReducer,
   {},
-  compose(
-    applyMiddleware(sagaMiddleware),
-    reduxDevTools
-  )
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
 // then run the saga
